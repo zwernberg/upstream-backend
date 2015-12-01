367daea4997d90e2072cf7856705c93d7466863d
Zach Wernberg <zawerny@gmail.com>
2015-11-30 20:12:06 -0600

    update catch url with basename

--- core/urls.py
+++ core/urls.py
@@ -7,16 +7,20 @@ from rest_framework.authtoken import views as authViews
 
 from django.views.generic.base import RedirectView
 from django.views.generic import TemplateView
+from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'catches', views.CatchViewSet)
+router.register(r'catches', views.CatchViewSet,base_name='catch')
+#router.register(r'feed',views.FeedViewSet,base_name='feed')
 
 urlpatterns = patterns('',
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', 
 	namespace='rest_framework')),
-	url(r'^api-token-auth', authViews.obtain_auth_token) 
+	url(r'^api-token-auth', authViews.obtain_auth_token),
+	url(r'^admin/', include(admin.site.urls)),
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
