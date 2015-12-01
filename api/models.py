384bfa390ef41143657a8463d243a728441c39b6
Zach Wernberg <zawerny@gmail.com>
2015-11-30 19:55:01 -0600

    updated models for stream_django

--- api/models.py
+++ api/models.py
@@ -5,18 +5,40 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.conf import settings
 from django.db.models.signals import post_save
-
+from stream_django import activity
 
 # Create your models here.
-class Catch(models.Model):
+class Catch(models.Model, activity.Activity):
 	title = models.CharField(max_length=75)
 	fishPhoto = models.ImageField(upload_to='photos/%Y/%m/%d')
 	owner = models.ForeignKey('auth.User', related_name='catches')
-	created_date = models.DateTimeField(auto_now_add=True)
-	modified_date = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	modified_at = models.DateTimeField(auto_now=True)
 
 	def __unicode_(self):
 		return self.title
+		
+	@property
+	def activity_actor_attr(self):
+		return self.owner
+		
+	@property
+	def activity_object_attr(self):
+		return self
+
+	@property
+	def activity_time_attr(self):
+		return self.created_date
+
+class Follow(models.Model):
+	user = models.ForeignKey('auth.User', related_name='friends')
+	target = models.ForeignKey('auth.User', related_name='followers')
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+	    unique_together = ('user', 'target')
+
+	
 		
 	
 # This code is triggered whenever a new user has been created and saved to the database
