e12814502c31c11864b9d9cfb8b69ee81e289942
Zach Wernberg <zawerny@gmail.com>
2016-02-04 20:11:38 -0600

    add back pagination

--- core/settings.py
+++ core/settings.py
@@ -107,8 +107,8 @@ REST_FRAMEWORK = {
 		'rest_framework.authentication.TokenAuthentication',
 		'rest_framework.authentication.SessionAuthentication',
     ),
-	##'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-	##'PAGE_SIZE': 10
+	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+	'PAGE_SIZE': 10
 
 
 }
