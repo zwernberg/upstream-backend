17f1aad28fa89cbdc2e68e9e89692efa94c1dd13
Zach Wernberg <zawerny@gmail.com>
2016-04-11 21:58:48 -0500

    add userprofile post_save signal

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -8,3 +9,13 @@ class UserProfile(models.Model):
 	
 	#custom userProfile Fields
 	bio = models.TextField(default='', blank=True)
+
+
+
+#Signals
+def create_profile(sender, **kwargs):
+	user = kwargs["instance"]
+	if kwargs["created"]:
+		up = UserProfile(user=user)
+		up.save()
+post_save.connect(create_profile, sender=User)
